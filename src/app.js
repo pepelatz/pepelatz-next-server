f7ee13ad61c856f41110acd5f483720027abaa7e
Vlad Zadvorny <vladzadvorny@icloud.com>
2018-08-06 17:44:00 +0300

    update apollo server

--- src/app.js
+++ src/app.js
@@ -1,38 +1,26 @@
 import Koa from 'koa';
-import Router from 'koa-router';
 import koaBody from 'koa-bodyparser';
 import cors from '@koa/cors';
 import helmet from 'koa-helmet';
-import { makeExecutableSchema } from 'graphql-tools';
-import { graphqlKoa, graphiqlKoa } from 'apollo-server-koa';
+import { ApolloServer } from 'apollo-server-koa';
 
 import typeDefs from './schema.gql';
 import resolvers from './resolvers';
 import { endpointURL, isDevelopment } from './utils/config';
 
-const schema = makeExecutableSchema({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: {},
+  debug: isDevelopment
+});
 
 const app = new Koa();
-const router = new Router();
 
 app.use(helmet());
 app.use(koaBody());
 app.use(cors());
 
-router.all(
-  endpointURL,
-  // eslint-disable-next-line
-  graphqlKoa(ctx => ({
-    schema,
-    context: {},
-    debug: isDevelopment
-  }))
-);
-
-if (isDevelopment) {
-  router.get('/graphiql', graphiqlKoa({ endpointURL }));
-}
-
-app.use(router.routes()).use(router.allowedMethods());
+server.applyMiddleware({ app, path: endpointURL });
 
 export default app;
